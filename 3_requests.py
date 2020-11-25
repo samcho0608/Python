d0d9cafe39887159ecdbafe34c5bd2003db8ef43
samcho0608 <organicyellow0608@gmail.com>
2020-11-26 01:37:21 +0900

    learned basics of pyinstaller and web structure

---
+++ 3_requests.py
@@ -0,0 +1,19 @@
+import requests
+res = requests.get("http://google.com") # successful
+# res = requests.get("http://nadocoding1.tistory.com") # erroneous
+
+print("Response Code :", res.status_code) # if 200, accessed successfully
+
+# if res.status_code == requests.codes.ok:    # if accessed successfully
+#     print("정상입니다.")
+# else:
+#     print("문제가 생겼습니다. [에러코드 ", res.status_code, "]")
+
+res.raise_for_status()  # stops the program is the access wasn't successful
+# print("웹 스크래핑을 진행합니다.")
+
+print(len(res.text))
+print(res.text)
+
+with open("mygoogle.html", "w", encoding="utf8") as f:
+    f.write(res.text)
